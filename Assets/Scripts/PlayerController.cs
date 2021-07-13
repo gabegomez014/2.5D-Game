a689a003126a9b6bdf5feb5a762503d02abfad0e
gabegomez014 <gomezagabriel2@gmail.com>
2021-07-13 18:58:19 -0400

    Finished the introduction section

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _jumpForce = 5;
+    private Rigidbody _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (_rb.velocity.y == 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * _jumpForce);
+        }
+        
+    }
+}
